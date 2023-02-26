87da12d92f0e41e6146a72a7250e1805838a0a57
MiAldoesc <126296668+MiAldoesc@users.noreply.github.com>
2023-02-25 19:31:42 -0600

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//crear la variable player con el sprite del jugador
+
+//la funcion setup se hace al inicio del juego
+function setup(){
+  //el createcanvas sirve para hacer la pantalla donde estara nuestro juego
+  createCanvas(400,400)
+  //crear sprite (objeto) con la variable player
+  
+}
+//la funcion draw se repite durante todo el juego
+function draw(){
+  //el background establece un fondo
+  //255=blanco
+  //0=negro
+  //https://html-color.codes/ si quieres buscar otro #??????
+  background(255)
+  //el drawsprites sirve para que los sprites se vean
+  //hacer una condicion if para que se pueda mover el jugador a la derecha
+  
+  //hacer una condicion if para que se pueda mover el jugador a la izquierda
+  
+  //hacer una condicion if para que se pueda mover el jugador a arriba
+  
+  //hacer una condicion if para que se pueda mover el jugador a abajo
+  drawSprites()
+}
